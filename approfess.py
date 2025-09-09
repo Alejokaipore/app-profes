aa4eae6c4fa0073af33195a2642e52873c1cd414
alejokaipore <alejandrorodriguez@gimnasiokaipore.com>
2025-09-08 19:31:47 -0500

    Se cambia origen de lectura de lista de estudiantes para subir notas

--- approfess.py
+++ approfess.py
@@ -1171,18 +1171,11 @@ with col2:
 
     st.write(f"Fecha del registro: {fecha_actual}")
 
-    # --- Lista de estudiantes y grado desde la base ---
-    conn = get_connection()
-    cursor = conn.cursor()
-    cursor.execute("SELECT estudiante, grado FROM estudiantes ORDER BY estudiante")
-    data = cursor.fetchall()
-    cursor.close()
-    conn.close()
 
    # --- Formulario ---
     with st.form("formulario_estudiante"):
         # Diccionario {estudiante: grado}
-        estudiantes_dict = {row[0]: row[1] for row in data}
+        estudiantes_dict = {row[0]: row[1] for row in estudiantes}
 
         # Lista de estudiantes para selectbox
         estudiante_seleccionado = st.selectbox("", list(estudiantes_dict.keys()))
