7c34fbac4d95f4043f0a6f6eab0e589a6352c217
Alejokaipore <alejandrorodriguez@gimnasiokaipore.com>
2025-10-03 16:09:55 -0500

    Se cambia el archivo db_utils apra tener los tres archivos principales

--- db_utils.py
+++ db_utils.py
@@ -1,12 +1,75 @@
 # db_utils.py
 import streamlit as st
-import mysql.connector
+import pandas as pd
+from sqlalchemy import create_engine
 
-def get_connection():
-    return mysql.connector.connect(
-        host=st.secrets["mysql"]["host"],
-        port=st.secrets["mysql"]["port"],
-        user=st.secrets["mysql"]["user"],
-        password=st.secrets["mysql"]["password"],
-        database=st.secrets["mysql"]["database"]
+host=st.secrets["mysql"]["host"],
+port=st.secrets["mysql"]["port"],
+user=st.secrets["mysql"]["user"],
+password=st.secrets["mysql"]["password"],
+database=st.secrets["mysql"]["database"]
+
+def crear_engine():
+    return create_engine(
+        f"mysql+pymysql://{user}:{password}@{host}:{port}/{database}",
+        connect_args={"ssl_ca": ssl_ca}
     )
+
+def obtener_notas_planetscale():
+   
+    query1 = """
+        SELECT 
+            n.fecha,
+            n.anio,
+            e.estudiante,
+            n.grado,
+            n.docente,
+            a.asignatura,
+            n.bloque,
+            n.periodo,
+            n.semana,
+            n.etapa,
+            n.calificacion
+        FROM notas n
+        JOIN estudiantes e ON e.codigo = n.codigo_estudiante
+        JOIN asignaturas a ON a.codigo = n.codigo_asignatura
+        LIMIT 700
+        """
+    query2 = """
+        SELECT 
+            n.fecha,
+            n.anio,
+            e.estudiante,
+            n.grado,
+            n.docente,
+            a.asignatura,
+            n.bloque,
+            n.periodo,
+            n.semana,
+            n.etapa,
+            n.calificacion
+        FROM notas n
+        JOIN estudiantes e ON e.codigo = n.codigo_estudiante
+        JOIN asignaturas a ON a.codigo = n.codigo_asignatura
+        LIMIT 100000 OFFSET 700
+        """
+    
+    engine = create_engine(
+        f"mysql+pymysql://{user}:{password}@{host}:{port}/{database}",
+        connect_args={"ssl_ca": ssl_ca}
+    )
+    with engine.connect() as conn:
+        df1 = pd.read_sql(query1, conn)
+        df2 = pd.read_sql(query2, conn)
+    df = pd.concat([df1, df2], ignore_index=True)
+    return df
+
+def listado_general_planetscale():
+    query = "SELECT estudiante, grupo, grado, dg, correo, meta FROM estudiantes"
+    engine = create_engine(
+        f"mysql+pymysql://{user}:{password}@{host}:{port}/{database}",
+        connect_args={"ssl_ca": ssl_ca}
+    )
+    with engine.connect() as conn:
+        df = pd.read_sql(query, conn)
+    return df
